7bc74aff65f6c54c0b45ade0adc97085fc4d98d6
Mohammad Ayaan Siddiqui <ayaangames@gmail.com>
2022-11-16 01:41:23 +0530

    added navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,9 +6,13 @@ const Navbar = () => {
       <a href="/" class="active">
         Home
       </a>
-      <a href="#contact">GITHUB Repo</a>
+      <a href="https://github.com/moayaan1911/gen-p" target={"_blank"}>
+        GITHUB Repo
+      </a>
       <a href="#about">Video Demo</a>
-      <a href="https://linktr.ee/ayaaneth">✨Connect With Developer✨</a>
+      <a href="https://linktr.ee/ayaaneth" target="_blank" rel="noreferrer">
+        ✨Connect With Developer✨
+      </a>
       <a href="javascript:void(0);" class="icon" onclick="myFunction()">
         <i class="fa fa-bars"></i>
       </a>
