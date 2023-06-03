2272564b2b3345a8c9351970911b204d2e0039b1
moayaan1911 <ayaangames@gmail.com>
2023-06-04 05:15:27 +0530

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "./Navbar";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Navbar />
     <App />
     <Toaster></Toaster>
+    <Analytics />
   </React.StrictMode>
 );
